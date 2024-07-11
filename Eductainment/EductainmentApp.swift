bd8cebc9221cfd49666c5f5084ec79b8db493fbb
chef-nic <nicholasajohnson@me.com>
2024-07-11 15:16:13 -0700

    Initial Commit

---
+++ Eductainment/EductainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EductainmentApp.swift
+//  Eductainment
+//
+//  Created by Nicholas Johnson on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EductainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
